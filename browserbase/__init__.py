b2ae86c0a16a59e2df02975a753fb30c98f3f794
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-04-09 19:42:28 +0200

    Formatted with ruff

--- browserbase/__init__.py
+++ browserbase/__init__.py
@@ -1,23 +1,26 @@
 from playwright.sync_api import sync_playwright
 
+
 class Browserbase:
     # Create new Browserbase instance
     def __init__(self, api_key: str):
-      if not api_key:
-        raise ValueError("Browserbase API key was not provided")
+        if not api_key:
+            raise ValueError("Browserbase API key was not provided")
 
-      self.api_key = api_key
+        self.api_key = api_key
 
     # Load a page in a headless browser and return the html contents
     def load(self, url: str):
         if not url:
-          raise ValueError("Page URL was not provided")
+            raise ValueError("Page URL was not provided")
 
         with sync_playwright() as p:
-          browser = p.chromium.connect_over_cdp("wss://api.browserbase.com?apiKey=" + self.api_key)
-          page = browser.new_page()
-          page.goto(url)
-          html = page.content()
-          browser.close()
+            browser = p.chromium.connect_over_cdp(
+                "wss://api.browserbase.com?apiKey=" + self.api_key
+            )
+            page = browser.new_page()
+            page.goto(url)
+            html = page.content()
+            browser.close()
 
-          return html
+            return html
--- tests/lib.py
+++ tests/lib.py
@@ -2,13 +2,15 @@ import unittest
 import os
 from src.browserbase import Browserbase
 
+
 class BrowserbaseTestCase(unittest.TestCase):
     def setUp(self):
         self.browserbase = Browserbase(os.environ["BROWSERBASE_KEY"])
 
     def test_load(self):
-        result = self.browserbase.load('https://example.com')
-        self.assertIn('Example Domain', result)
+        result = self.browserbase.load("https://example.com")
+        self.assertIn("Example Domain", result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
